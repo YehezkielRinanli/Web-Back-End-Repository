69fe843e4e81865ddbb1712ad5ee565c5932155b
Yehezkiel <yehezkielsaragih4@gmail.com>
2026-07-03 10:45:14 +0700

    fixing and developing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from '../models/user.js';
+import User from '../models/User.js';
 import fs from "fs";
 
 export const getAllUsers = async (req, res, next) => {
--- config/database.js
+++ config/database.js
@@ -1,11 +1,19 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-//sesuaikan nama database kita dgn yg dibawah, dan untuk password boleh diubah saat pull
-//sebelum push kosongkan bagian password database nya untuk mempermudah kita
-const db = new Sequelize('project_uts_db', 'root', '150605', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
-});
+// Mengaktifkan dotenv untuk membaca environment variable
+dotenv.config();
+
+const db = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+        logging: false,
+    }
+);
 
 export default db;
